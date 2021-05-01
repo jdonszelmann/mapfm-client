8db4b56cf2984dce3d278d9a1e6f15f6698de5a1
jonay2000 <jonabent@gmail.com>
2021-05-01 10:31:36 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="mapfmclient",
-    version="0.0.9",
+    version="0.0.10",
     author="Jonathan D�nszelmann",
     author_email="jonabent@gmail.com",
     description="client library for interaction with mapf.nl",
